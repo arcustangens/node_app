87d05b197835b0847c27a5df112c83cb573a1a64
WOJCIECH KLEMENS <wojtekklemens@gmail.com>
2021-12-22 13:57:16 +0100

    Handle database setup and insert request

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const res = require("express/lib/response");
-const app = express();
+const express = require("express")
+const bodyParser = require("body-parser")
+const res = require("express/lib/response")
+const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const database = require("./database.js")
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function () {
-  console.log("listening on 3000");
-});
+  console.log("listening on 3000")
+})
 app.post("/form", (req, res) => {
-  res.send(req.body);
-});
+  res.send(req.body)
+})
+app.get("/test", (req, res) => {
+  const conn = database.establishConnection().then(conn => {
+    console.log("fajnie")
+    conn
+      .query("INSERT INTO materialy value (?, ?)", [
+        null,
+        "testowy material szzefie",
+      ])
+      .then(result => console.log(result))
+  })
+})
