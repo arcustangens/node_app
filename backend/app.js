6a52fa4b09ef5c795c0b2151888227a52b3f0578
Paweł Mendroch <pawelmendroch7@gmail.com>
2021-12-22 14:09:40 +0100

    Top level async await and connection var

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,30 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const res = require("express/lib/response")
+const express = require('express')
+const bodyParser = require('body-parser')
+const res = require('express/lib/response')
 const app = express()
 
-const database = require("./database.js")
+const database = require('./database.js')
+
+let conn
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, function () {
-  console.log("listening on 3000")
-})
-app.post("/form", (req, res) => {
+app.post('/form', (req, res) => {
   res.send(req.body)
 })
-app.get("/test", (req, res) => {
-  const conn = database.establishConnection().then(conn => {
-    console.log("fajnie")
-    conn
-      .query("INSERT INTO materialy value (?, ?)", [
-        null,
-        "testowy material szzefie",
-      ])
-      .then(result => console.log(result))
-  })
+app.get('/test', async (req, res) => {
+  const response = await conn.query('INSERT INTO materialy value (?, ?)', [
+    null,
+    'testowy material szzefie',
+  ])
+  console.log(response)
 })
+
+const main = async () => {
+  conn = await database.establishConnection()
+  app.listen(3000, function () {
+    console.log('listening on 3000')
+  })
+}
+
+main()
