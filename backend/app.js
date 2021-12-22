edd3b9365308fac571dad71386ce0cfc59a0e8bc
Paweł Mendroch <pawelmendroch7@gmail.com>
2021-12-22 18:00:47 +0100

    Fix backend get endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,76 +1,85 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const res = require("express/lib/response")
+const express = require('express')
+const bodyParser = require('body-parser')
+const res = require('express/lib/response')
 const app = express()
 
-const database = require("./database.js")
+const database = require('./database.js')
 
 let conn
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.post("/form", (req, res) => {
+app.post('/form', (req, res) => {
   console.log(req.body)
   res.send()
 })
 
-app.get("/kontrahenci", async (req, res) => {
+app.get('/kontrahenci', async (req, res) => {
   try {
-    response = await conn.query("SELECT id, kontrahent FROM kontrahenci;")
-    output = { value: response.id, label: response.kontrahent }
-    res.send(output)
+    const data = await conn.query('SELECT * FROM materialy;')
+    const parsedData = data.map(({ id, kontrahent }) => ({
+      value: id,
+      label: kontrahent,
+    }))
+    res.send(parsedData)
   } catch (err) {
-    console.log(err)
+    res.status(404).send(err)
   }
 })
-app.post("/kontrahenci", async (req, res) => {
+app.post('/kontrahenci', async (req, res) => {
   conn
-    .query("INSERT INTO kontrahenci value (?, ?, ?)", [
+    .query('INSERT INTO kontrahenci value (?, ?, ?)', [
       null,
       req.body.kontrahent,
-      "TEST",
+      'TEST',
     ])
     .then(res.send())
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err))
 })
 
-app.get("/materialy", async (req, res) => {
+app.get('/materialy', async (req, res) => {
   try {
-    response = await conn.query("SELECT * FROM materialy;")
-    output = { value: response.id, label: response.material }
-    res.send(output)
+    const data = await conn.query('SELECT * FROM materialy;')
+    const parsedData = data.map(({ id, material }) => ({
+      value: id,
+      label: material,
+    }))
+    res.send(parsedData)
   } catch (err) {
-    console.log(err)
+    res.status(404).send(err)
   }
 })
-app.post("/materialy", async (req, res) => {
+app.post('/materialy', async (req, res) => {
   conn
-    .query("INSERT INTO materialy value (?, ?)", [null, req.body.material])
+    .query('INSERT INTO materialy value (?, ?)', [null, req.body.material])
     .then(res.send())
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err))
 })
 
-app.get("/typ_wymiaru", async (req, res) => {
+app.get('/typ_wymiaru', async (req, res) => {
   try {
-    response = await conn.query("SELECT * FROM typ_wymiaru;")
-    output = { value: response.id, label: response.typ }
-    res.send(output)
+    const data = await conn.query('SELECT * FROM typ_wymiaru;')
+    const parsedData = data.map(({ id, typ }) => ({
+      value: id,
+      label: typ,
+    }))
+    res.send(parsedData)
   } catch (err) {
-    console.log(err)
+    res.status(404).send(err)
   }
 })
-app.post("/typ_wymiaru", async (req, res) => {
+app.post('/typ_wymiaru', async (req, res) => {
   conn
-    .query("INSERT INTO typ_wymiaru value (?, ?)", [null, req.body.typWymiaru])
+    .query('INSERT INTO typ_wymiaru value (?, ?)', [null, req.body.typWymiaru])
     .then(res.send())
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err))
 })
 
 const main = async () => {
   conn = await database.establishConnection()
   app.listen(3000, function () {
-    console.log("listening on 3000")
+    console.log('listening on 3000')
   })
 }
 
