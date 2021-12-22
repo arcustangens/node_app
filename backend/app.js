484486b7125a945c8b3dad0884e5af96f6df9263
WOJCIECH KLEMENS <wojtekklemens@gmail.com>
2021-12-22 16:45:10 +0100

    refactor

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const res = require('express/lib/response')
+const express = require("express")
+const bodyParser = require("body-parser")
+const res = require("express/lib/response")
 const app = express()
 
-const database = require('./database.js')
+const database = require("./database.js")
 
 let conn
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/form', (req, res) => {
+app.post("/form", (req, res) => {
   res.send(req.body)
 })
-app.get('/test', async (req, res) => {
-  const response = await conn.query('INSERT INTO materialy value (?, ?)', [
+app.get("/test", async (req, res) => {
+  const response = await conn.query("INSERT INTO materialy value (?, ?)", [
     null,
-    'testowy material szzefie',
+    "testowy material szzefie",
   ])
   console.log(response)
 })
@@ -23,7 +23,7 @@ app.get('/test', async (req, res) => {
 const main = async () => {
   conn = await database.establishConnection()
   app.listen(3000, function () {
-    console.log('listening on 3000')
+    console.log("listening on 3000")
   })
 }
 
