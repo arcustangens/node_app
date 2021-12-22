4d08bcbc2097f4eebe453bc42d63f92cdb2775fa
WOJCIECH KLEMENS <wojtekklemens@gmail.com>
2021-12-22 16:54:17 +0100

    Expand routing

--- backend/app.js
+++ backend/app.js
@@ -10,14 +10,34 @@ let conn
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post("/form", (req, res) => {
-  res.send(req.body)
+  console.log(req.body)
+  res.send()
 })
-app.get("/test", async (req, res) => {
-  const response = await conn.query("INSERT INTO materialy value (?, ?)", [
-    null,
-    "testowy material szzefie",
-  ])
-  console.log(response)
+app.get("/kontrahenci", async (req, res) => {
+  conn
+    .query("SELECT * FROM kontrahenci;")
+    .then(response => {
+      console.log(response)
+      res.send(response)
+    })
+    .catch(err => console.log(err))
+})
+app.post("/kontrahenci", async (req, res) => {
+  console.log(req.body)
+  res.send()
+})
+app.get("/materialy", async (req, res) => {
+  conn
+    .query("SELECT * FROM materialy;")
+    .then(response => {
+      console.log(response)
+      res.send(response)
+    })
+    .catch(err => console.log(err))
+})
+app.post("/materialy", async (req, res) => {
+  console.log(req.body)
+  res.send()
 })
 
 const main = async () => {
