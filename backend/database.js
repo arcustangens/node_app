687ac46ea04de30da4c82d723af1ced5e7a8508c
WOJCIECH KLEMENS <wojtekklemens@gmail.com>
2021-12-22 17:47:57 +0100

    Change database structure

--- backend/database.js
+++ backend/database.js
@@ -20,7 +20,7 @@ async function establishConnection(
     await conn.query("create database if not exists demo;")
     await conn.query("use demo;")
     await conn.query(
-      "CREATE TABLE IF NOT EXISTS `records` ( `id` int(11) NOT NULL AUTO_INCREMENT, `kontrahent` varchar(255) DEFAULT NULL, `numer` varchar(255) DEFAULT NULL, `typ_wymiaru` varchar(255) DEFAULT NULL, `x` int(11) DEFAULT NULL, `y` int(11) DEFAULT NULL, `z` int(11) DEFAULT NULL, `nazwa` varchar(255) DEFAULT NULL, `material` varchar(255) DEFAULT NULL, PRIMARY KEY (`id`) )"
+      "CREATE TABLE IF NOT EXISTS `records` ( `id` int(11) NOT NULL AUTO_INCREMENT, `id_kontrahenta` int DEFAULT NULL, `numer` varchar(255) DEFAULT NULL, `id_typu_wymiaru` int DEFAULT NULL, `a` int(11) DEFAULT NULL, `b` int(11) DEFAULT NULL, `c` int(11) DEFAULT NULL, `d` int(11) DEFAULT NULL, `e` int(11) DEFAULT NULL, `f` int(11) DEFAULT NULL, `nazwa` varchar(255) DEFAULT NULL, `id_materialu` varchar(255) DEFAULT NULL, `uwagi` varchar(255) DEFAULT NULL, PRIMARY KEY (`id`) )"
     )
     await conn.query(
       "CREATE TABLE IF NOT EXISTS `kontrahenci` ( `id` int(11) NOT NULL AUTO_INCREMENT, `kontrahent` varchar(255) DEFAULT NULL, `akronim` varchar(255) DEFAULT NULL, PRIMARY KEY (`id`) )"
